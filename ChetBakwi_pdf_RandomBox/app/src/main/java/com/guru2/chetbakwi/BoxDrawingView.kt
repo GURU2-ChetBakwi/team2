72e7c6b8bda42953c195ecc35bfa65d19568b7c7
Sojin Jang <dpreallove@gmail.com>
2023-07-30 03:48:36 +0900

    pdf randombox 페이지별 구현 완료

--- ChetBakwi_pdf_RandomBox/app/src/main/java/com/guru2/chetbakwi/BoxDrawingView.kt
+++ ChetBakwi_pdf_RandomBox/app/src/main/java/com/guru2/chetbakwi/BoxDrawingView.kt
@@ -7,7 +7,6 @@ import android.graphics.Paint
 import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.MotionEvent
-import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatImageView
@@ -16,53 +15,53 @@ class BoxDrawingView(context: Context, attrs: AttributeSet) : AppCompatImageView
     // 박스를 그리기 위한 Paint 객체
     private val boxPaint: Paint = Paint()
 
-    //tvBoxCount를 멤버변수로 선언
+    // 빈칸 수를 표시하기 위한 TextView
     private lateinit var tvBoxCount: TextView
 
-    // 페이지별로 생성된 박스들을 저장하는 리스트
-    private val boxesByPage: MutableList<MutableList<RectF>> = mutableListOf()
+    // 현재 페이지 인덱스
     private var currentPageIndex: Int = 0
 
-    // 추가!! 랜덤으로 생성된 상자를 저장할 목록
-    private val randomBoxes: MutableList<RectF> = mutableListOf()
+    // 페이지별로 생성된 드래그 박스들을 저장하는 리스트
+    private val boxesByPage: MutableList<MutableList<RectF>> = mutableListOf()
 
-    // Setter method for tvBoxCount
+    // 페이지별로 생성된 랜덤 상자를 저장하는 리스트
+    private val randomBoxesByPage: MutableList<MutableList<RectF>> = mutableListOf()
+
+    // tvBoxCount를 설정하는 Setter 메서드
     fun setTvBoxCount(tvBoxCount: TextView) {
         this.tvBoxCount = tvBoxCount
     }
 
     init {
         // 박스의 스타일과 두께, 색상 설정
-        boxPaint.style = Paint.Style.FILL_AND_STROKE
+        boxPaint.style = Paint.Style.FILL
         boxPaint.strokeWidth = 5f
-        boxPaint.color = Color.RED
+        boxPaint.color = Color.parseColor("#FF6B6B")
+
+        // 초기 페이지별 리스트들을 빈 리스트로 초기화
         boxesByPage.add(mutableListOf())
+        randomBoxesByPage.add(mutableListOf())
     }
 
     // View의 그리기 메서드를 오버라이드하여 박스 그리기
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
-        // 추가!! 랜덤 박스 그리기
-        randomBoxes.forEach { box ->
-            canvas.drawRect(box, boxPaint)
-        }
-
         // 현재 페이지 인덱스에 해당하는 박스들만 그림
         val currentPageBoxes = boxesByPage.getOrNull(currentPageIndex)
         currentPageBoxes?.forEach { box ->
-            canvas.drawRect(box, boxPaint)
+            canvas.drawRoundRect(box, 20f, 20f, boxPaint) // 둥근 테두리로 그리기
         }
 
-        // 추가!! 랜덤 박스 그리기
-        randomBoxes.forEach { box ->
-            canvas.drawRect(box, boxPaint)
+        // 현재 페이지 인덱스에 해당하는 랜덤 박스들만 그림
+        val currentPageRandomBoxes = randomBoxesByPage.getOrNull(currentPageIndex)
+        currentPageRandomBoxes?.forEach { box ->
+            canvas.drawRoundRect(box, 20f, 20f, boxPaint) // 둥근 테두리로 그리기
         }
 
-        // 추가!! 상자의 총 개수 계산(사용자가 그린 + 랜덤)
-        val totalBoxCount = (currentPageBoxes?.size ?: 0) + randomBoxes.size
+        // 상자의 총 개수 계산(사용자가 그린 + 랜덤)
+        val totalBoxCount = (currentPageBoxes?.size ?: 0) + (currentPageRandomBoxes?.size ?: 0)
         tvBoxCount.text = "남은 빈칸 수: $totalBoxCount"
-
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
@@ -72,8 +71,9 @@ class BoxDrawingView(context: Context, attrs: AttributeSet) : AppCompatImageView
                 val x = event.x
                 val y = event.y
 
-                // 현재 페이지 인덱스에 해당하는 상자만 가져옴
+                // 현재 페이지 인덱스에 해당하는 상자 리스트들 가져오기
                 val currentPageBoxes = boxesByPage.getOrNull(currentPageIndex)
+                val currentPageRandomBoxes = randomBoxesByPage.getOrNull(currentPageIndex)
 
                 if (currentPageBoxes != null) {
                     // 이미 생성된 박스 중에 선택된 박스가 있는지 확인
@@ -92,27 +92,33 @@ class BoxDrawingView(context: Context, attrs: AttributeSet) : AppCompatImageView
                         currentPageBoxes.removeAt(selectedBoxIndex)
                     } else {
                         // 터치 이벤트가 임의로 생성된 상자에 속하는지 확인
+                        val currentPageRandomBoxes = randomBoxesByPage.getOrNull(currentPageIndex)
+
+                        // currentPageRandomBoxes가 null인 경우 빈 리스트로 대체
+                        val currentPageBoxesList = currentPageRandomBoxes ?: emptyList()
+
                         var selectedRandomBoxIndex = -1
-                        for (i in randomBoxes.size - 1 downTo 0) {
-                            if (randomBoxes[i].contains(x, y)) {
+                        for (i in currentPageBoxesList.size - 1 downTo 0) {
+                            if (currentPageBoxesList[i].contains(x, y)) {
                                 selectedRandomBoxIndex = i
                                 break // 최상위 상자를 찾고 break 문으로 종료
                             }
                         }
 
+
                         if (selectedRandomBoxIndex != -1) {
                             // 선택된 임의의 상자가 있으면 해당 상자를 삭제
-                            randomBoxes.removeAt(selectedRandomBoxIndex)
+                            currentPageRandomBoxes?.removeAt(selectedRandomBoxIndex)
                         } else {
                             // 새로운 박스 생성
-                            val box = RectF(x, y, x + 100, y + 100) // Create a box with width and height of 100
+                            val box = RectF(x, y, x + 100, y + 100)
                             currentPageBoxes.add(box)
                         }
                     }
                 } else {
                     // 현재 페이지에 박스 리스트가 없다면 새로 생성
                     val newBoxesList = mutableListOf<RectF>()
-                    val box = RectF(x, y, x + 100, y + 100) // Create a box with width and height of 100
+                    val box = RectF(x, y, x + 100, y + 100)
                     newBoxesList.add(box)
                     boxesByPage.add(newBoxesList)
                 }
@@ -140,71 +146,101 @@ class BoxDrawingView(context: Context, attrs: AttributeSet) : AppCompatImageView
     // 현재 페이지 인덱스 설정
     fun setCurrentPageIndex(index: Int) {
         currentPageIndex = index
-    }
-
-    // 박스들을 모두 지우고 화면을 다시 그리도록 호출하는 함수
-    fun clearBoxes() {
-        val currentPageBoxes = boxesByPage.getOrNull(currentPageIndex)
-
-        if (currentPageBoxes != null && currentPageBoxes.isEmpty() && randomBoxes.isEmpty()) {
-            // 드래그로 생성된 박스가 없다면 토스트 메시지를 출력
-            Toast.makeText(context, "지울 상자가 없습니다.", Toast.LENGTH_SHORT).show()
-        } else {
-            // 드래그하여 생성된 상자를 지웁니다.
-            currentPageBoxes?.clear()
-
-            // 이전에 무작위로 생성된 빈 상자를 제거합니다.
-            randomBoxes.clear()
-
-            // 화면을 다시 그리기 위해 호출
-            invalidate()
+        // 페이지 인덱스에 해당하는 랜덤 박스 리스트가 없으면 빈 리스트를 추가
+        while (randomBoxesByPage.size <= index) {
+            randomBoxesByPage.add(mutableListOf())
         }
         updateBoxCount()
-    }
-
-    // 페이지를 넘길 때 랜덤 박스 초기화
-    fun clearRandomBoxes() {
-        randomBoxes.clear()
-        invalidate()
-    }
-
-    // 페이지별 박스 리스트 초기화
-    fun clearAllBoxes() {
-        boxesByPage.clear()
-        randomBoxes.clear()
         invalidate()
     }
 
     // 랜덤한 위치에 박스 생성하는 함수
-// 랜덤한 위치에 박스 생성하는 함수
     fun addRandomBoxes(count: Int) {
+        // 현재 페이지에 해당하는 랜덤 박스 리스트를 가져오기
+        val currentPageRandomBoxes = randomBoxesByPage.getOrNull(currentPageIndex)
+
         // 이전 박스들을 모두 제거
-        randomBoxes.clear()
+        currentPageRandomBoxes?.clear()
 
         // PDF 화면의 크기 가져오기
         val pdfViewWidth = width.toFloat()
         val pdfViewHeight = height.toFloat()
 
+        // 최대 시도 횟수를 설정합니다.
+        val maxAttempts = 100
+
         // 입력된 개수만큼 랜덤한 박스 생성
         for (i in 1..count) {
             // 랜덤한 x, y 좌표를 생성
-            val randomX = (0..((pdfViewWidth - 100).toInt())).random().toFloat()
-            val randomY = (0..((pdfViewHeight - 620).toInt())).random().toFloat() + 300f
+            var randomX = (0..((pdfViewWidth - 100).toInt())).random().toFloat()
+            var randomY = (0..((pdfViewHeight - 620).toInt())).random().toFloat() + 300f
 
-            // 빨간색 랜덤 박스 추가
-            val box = RectF(randomX, randomY, randomX + 100, randomY + 20)
-            boxPaint.color = Color.RED // Set box color to red
-            randomBoxes.add(box)
-            updateBoxCount()
+            // 새로운 박스 생성
+            val newBox = RectF(randomX, randomY, randomX + 100, randomY + 25)
+            boxPaint.color = Color.parseColor("#FF6B6B") // 빨간색
+
+            // 기존의 박스들과 겹치는지 확인
+            var isOverlapping = false
+            for (box in currentPageRandomBoxes ?: emptyList()) {
+                if (RectF.intersects(box, newBox)) {
+                    isOverlapping = true
+                    break
+                }
+            }
+
+            // 겹치지 않을 때까지 다시 랜덤한 위치에 생성
+            while (isOverlapping) {
+                randomX = (0..((pdfViewWidth - 100).toInt())).random().toFloat()
+                randomY = (0..((pdfViewHeight - 620).toInt())).random().toFloat() + 300f
+                newBox.set(randomX, randomY, randomX + 100, randomY + 25)
+
+                isOverlapping = false
+                for (box in currentPageRandomBoxes ?: emptyList()) {
+                    if (RectF.intersects(box, newBox)) {
+                        isOverlapping = true
+                        break
+                    }
+                }
+            }
+
+            // 생성된 박스를 추가
+            currentPageRandomBoxes?.add(newBox)
         }
 
-        // 화면을 다시 그리도록 호출
+        // 상자 개수 업데이트 및 화면 다시 그리기
+        updateBoxCount()
         invalidate()
     }
-    private fun updateBoxCount() {
-        // Calculate the total number of boxes (drawn by user + random)
+
+    // 모든 상자들을 초기화하는 함수
+    fun clearAllBoxes() {
         val currentPageBoxes = boxesByPage.getOrNull(currentPageIndex)
-        val totalBoxCount = (currentPageBoxes?.size ?: 0) + randomBoxes.size
+        val currentPageRandomBoxes = randomBoxesByPage.getOrNull(currentPageIndex)
+
+        // 현재 페이지의 박스 리스트가 비어있는지 확인
+        val isBoxesEmpty = currentPageBoxes?.isEmpty() ?: true
+        val isRandomBoxesEmpty = currentPageRandomBoxes?.isEmpty() ?: true
+
+        // 만약 박스 리스트가 비어있다면 토스트 메시지
+        if (isBoxesEmpty && isRandomBoxesEmpty) {
+            Toast.makeText(context, "박스가 모두 초기화 되었습니다.", Toast.LENGTH_SHORT).show()
+        } else {
+            // 비어있지 않다면 박스 리스트를 초기화
+            currentPageBoxes?.clear()
+            currentPageRandomBoxes?.clear()
+            invalidate()
+
+            // 상자 개수 업데이트
+            updateBoxCount()
+        }
+    }
+
+    // 남은 빈칸 수를 업데이트하는 함수
+    private fun updateBoxCount() {
+        // 사용자 그림 상자와 랜덤 상자의 총 개수 계산
+        val currentPageBoxes = boxesByPage.getOrNull(currentPageIndex)
+        val currentPageRandomBoxes = randomBoxesByPage.getOrNull(currentPageIndex)
+        val totalBoxCount = (currentPageBoxes?.size ?: 0) + (currentPageRandomBoxes?.size ?: 0)
         tvBoxCount.text = "남은 빈칸 수 : $totalBoxCount"
     }
 }
--- ChetBakwi_pdf_RandomBox/app/src/main/java/com/guru2/chetbakwi/pdfViewPage.kt
+++ ChetBakwi_pdf_RandomBox/app/src/main/java/com/guru2/chetbakwi/pdfViewPage.kt
@@ -4,77 +4,60 @@ import android.app.AlertDialog
 import android.content.DialogInterface
 import android.content.Intent
 import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Rect
-import android.graphics.drawable.BitmapDrawable
 import android.graphics.pdf.PdfRenderer
 import android.net.Uri
 import android.os.Bundle
 import android.os.ParcelFileDescriptor
 import android.widget.Button
 import android.widget.EditText
-import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import java.io.File
 import java.io.FileOutputStream
-import com.googlecode.tesseract.android.TessBaseAPI
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import java.io.FileNotFoundException
 
 class pdfViewPage : AppCompatActivity() {
 
+    // 변수 선언
     private lateinit var pdfRenderer: PdfRenderer
     private lateinit var pdfFile: File
     private lateinit var currentPage: PdfRenderer.Page
 
-    // 추가
     private lateinit var btnPrev: Button
     private lateinit var btnNext: Button
     private lateinit var btnRandom: Button
     private lateinit var btnReset: Button
     private lateinit var pdfPageNumber: TextView
     private lateinit var etRandomBoxCount: EditText
+    private lateinit var tvBoxCount: TextView
+
     private lateinit var boxDrawingView: BoxDrawingView
+
     private var currentPageIndex: Int = 0
-    private lateinit var tesseract: TessBaseAPI
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pdfview)
 
-        //추가
+        // 초기화 및 연결
         btnPrev = findViewById(R.id.btnPrev)
         btnNext = findViewById(R.id.btnNext)
         btnRandom = findViewById(R.id.btnRandom)
         btnReset = findViewById(R.id.btnReset)
         etRandomBoxCount = findViewById(R.id.etRandomBoxCount)
         pdfPageNumber = findViewById(R.id.pdfPageNumber)
+        tvBoxCount = findViewById(R.id.tvBoxCount)
 
         // BoxDrawingView 초기화 및 연결
         boxDrawingView = findViewById(R.id.imageView)
-        val tvBoxCount: TextView = findViewById(R.id.tvBoxCount)
         boxDrawingView.setTvBoxCount(tvBoxCount)
 
-        val pdfPageNumber: TextView = findViewById(R.id.pdfPageNumber)
-        val boxDrawingView: BoxDrawingView = findViewById(R.id.imageView)
-
-
-
-        // tesseract 초기화
-        initTesseract()
-
-        // pdfUri 전달받기
+        // pdfUri를 인텐트로 전달받기
         val pdfUri: Uri = intent.data!!
         pdfFile = File(cacheDir, "temp.pdf")
         pdfFile.createNewFile()
 
+        // 선택된 PDF 파일을 임시 위치로 복사
         contentResolver.openInputStream(pdfUri)?.use { inputStream ->
             FileOutputStream(pdfFile).use { outputStream ->
                 val buffer = ByteArray(4 * 1024)
@@ -93,7 +76,6 @@ class pdfViewPage : AppCompatActivity() {
 
             // 첫 번째 페이지를 보여주기
             showPage(0)
-            processOcrForCurrentPage()
 
             //extractTextFromPDF(pdfUri)
         } else {
@@ -110,7 +92,6 @@ class pdfViewPage : AppCompatActivity() {
             alertDialog.show()
         }
 
-        //추가
         // 이전 페이지 버튼 클릭 리스너
         btnPrev.setOnClickListener {
             showPreviousPage()
@@ -137,17 +118,19 @@ class pdfViewPage : AppCompatActivity() {
             }
         }
 
-        // 빈칸 랜덤 생성 버튼 클릭 리스너
+        // 빈칸 초기화 버튼 클릭 리스너
         btnReset.setOnClickListener {
-            boxDrawingView.clearBoxes()
+            boxDrawingView.clearAllBoxes()
         }
     }
 
+    // PDF Renderer 열기
     private fun openRenderer() {
         val fileDescriptor = ParcelFileDescriptor.open(pdfFile, ParcelFileDescriptor.MODE_READ_ONLY)
         pdfRenderer = PdfRenderer(fileDescriptor)
     }
 
+    // 특정 페이지를 표시
     private fun showPage(index: Int) {
         if (index < 0 || index >= pdfRenderer.pageCount) {
             return
@@ -167,15 +150,12 @@ class pdfViewPage : AppCompatActivity() {
         pdfPageNumber.text = "${index + 1}/${pdfRenderer.pageCount}"
     }
 
-    //추가
     // 이전 페이지 보이기
     private fun showPreviousPage() {
         if (currentPageIndex > 0) {
             currentPageIndex--
             boxDrawingView.setCurrentPageIndex(currentPageIndex)
-            boxDrawingView.clearRandomBoxes() // 페이지를 넘길 때 랜덤 박스 초기화
             showPage(currentPageIndex)
-            processOcrForCurrentPage()
         } else {
             // 첫 페이지이므로 이전 페이지 없음을 알림
             Toast.makeText(this, "첫 페이지입니다.", Toast.LENGTH_SHORT).show()
@@ -187,9 +167,7 @@ class pdfViewPage : AppCompatActivity() {
         if (currentPageIndex < pdfRenderer.pageCount - 1) {
             currentPageIndex++
             boxDrawingView.setCurrentPageIndex(currentPageIndex)
-            boxDrawingView.clearRandomBoxes() // 페이지를 넘길 때 랜덤 박스 초기화
             showPage(currentPageIndex)
-            processOcrForCurrentPage()
         } else {
             // 마지막 페이지이므로 다음 페이지 없음을 알림
             Toast.makeText(this, "마지막 페이지입니다.", Toast.LENGTH_SHORT).show()
@@ -201,75 +179,5 @@ class pdfViewPage : AppCompatActivity() {
         // 액티비티가 종료될 때, 리소스를 정리
         currentPage.close()
         pdfRenderer.close()
-        tesseract.end()
-    }
-
-    private fun initTesseract() {
-        tesseract = TessBaseAPI()
-        // cacheDir.absolutePath : /data/data/com.guru2.chetbakwi/cache/tessdata
-        // 한국어와 영어를 동시에 인식 -> 추출
-        tesseract.init(cacheDir.absolutePath, "kor+eng")
-    }
-
-    // pdf의 모든 페이지에 있는 텍스트 추출
-    private fun extractTextFromPDF(pdfUri: Uri) {
-        GlobalScope.launch(Dispatchers.IO) {
-            val pdfText = withContext(Dispatchers.IO) {
-                try {
-                    // 읽기로 pdfuri 열기
-                    val pdfFileDescriptor: ParcelFileDescriptor? = contentResolver.openFileDescriptor(pdfUri, "r")
-                    if (pdfFileDescriptor != null) {
-                        // PDF에서 텍스트 추출
-                        val pdfTextBuilder = StringBuilder()
-                        val pdfRenderer = PdfRenderer(pdfFileDescriptor)
-                        // 첫번째 페이지부터 마지막 페이지까지
-                        for (pageIndex in 0 until pdfRenderer.pageCount) {
-                            // 페이지 열기
-                            val page = pdfRenderer.openPage(pageIndex)
-                            // 텍스트 추출
-                            val text = tesseractOCR(page)
-                            // textBulder에 append
-                            pdfTextBuilder.append(text)
-                            // 열어둔 페이지 닫기
-                            page.close()
-                        }
-                        // pdfRenderer 닫기
-                        pdfRenderer.close()
-                        pdfTextBuilder.toString()
-                    } else {
-                        "Error: Unable to open PDF file."
-                    }
-                } catch (e: FileNotFoundException) {
-                    e.printStackTrace()
-                    "Error: File not found."
-                }
-            }
-            // 추출된 pdfText print
-            println(pdfText)
-        }
-    }
-
-    // 현재 보이고 있는 페이지에서 텍스트 추출
-    private fun processOcrForCurrentPage() {
-        GlobalScope.launch(Dispatchers.IO) {
-            val pageText = withContext(Dispatchers.IO) {
-                tesseractOCR(currentPage)
-            }
-            // 추출된 텍스트를 이용해 필요한 처리 ------------------------------------------------- 생략 가능
-            println(pageText)
-        }
-    }
-
-    private fun tesseractOCR(page: PdfRenderer.Page): String {
-        // pdf 페이지 너비, 높이 기반 비트맵 생성 (pdf -> img)
-        val bitmap = Bitmap.createBitmap(page.width, page.height, Bitmap.Config.ARGB_8888)
-        // pdf 페이지를 전체 영역으로 렌더링 -> 비트맵에 그리기
-        val rect = Rect(0, 0, page.width, page.height)
-        page.render(bitmap, rect, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY)
-
-        // tesseract OCR 에 비트맵 설정 -> 이미지 인식
-        tesseract.setImage(bitmap)
-        // 인식된 텍스트 utf8로 변환 후 return
-        return tesseract.utF8Text
     }
 }
